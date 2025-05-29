3dac5dfae1a68e2c1acd1812c3751d9f3df75c59
abdulaziz_ulashbaev <abdulaziz_ulashbaev@student.itpu.uz>
2025-05-29 18:21:44 +0500

    model and repo successfully created for USER

---
+++ auth-service/src/main/java/uz/ulashbaev/authservice/model/User.java
@@ -0,0 +1,39 @@
+package uz.ulashbaev.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ auth-service/src/main/java/uz/ulashbaev/authservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package uz.ulashbaev.authservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.ulashbaev.authservice.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
