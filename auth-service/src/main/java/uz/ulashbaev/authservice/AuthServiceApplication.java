64b954c701d63f07bb1f97864281320a4f47f854
abdulaziz_ulashbaev <abdulaziz_ulashbaev@student.itpu.uz>
2025-05-29 22:44:34 +0500

    smth

---
+++ auth-service/src/main/java/uz/ulashbaev/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.ulashbaev.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
