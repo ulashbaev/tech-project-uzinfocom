ee25a431ae4415019916b333bfd4a72d734c9c75
abdulaziz_ulashbaev <abdulaziz_ulashbaev@student.itpu.uz>
2025-05-29 23:05:13 +0500

    added annation @EnableEurekaServer to DiscoveryServiceApplication, JwtAuthFilter updated in handling error 401

--- discovery-service/src/main/java/uz/ulashbaev/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/uz/ulashbaev/discoveryservice/DiscoveryServiceApplication.java
@@ -2,9 +2,10 @@ package uz.ulashbaev.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
